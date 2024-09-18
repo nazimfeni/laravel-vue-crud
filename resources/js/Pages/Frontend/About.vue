5064d8fddc360dd207bae1b4e5411de258d951da
nazimjrf <nazim@jrfintel.com>
2024-09-18 17:53:12 +0600

    updated

---
+++ resources/js/Pages/Frontend/About.vue
@@ -0,0 +1,14 @@
+<template>
+      <Frontend>
+            <Head title="Home page"/>
+            <h1>This is {{title}}</h1>
+      </Frontend>
+    
+</template>
+
+<script setup>
+      import Frontend from '@/Layouts/FrontendLayout.vue';
+      defineProps({
+            title:String
+      });
+</script>
---
+++ resources/js/Pages/Frontend/Home.vue
@@ -0,0 +1,15 @@
+<template>
+            
+           <Frontend>
+            <Head title="Home page"/>
+            <h1>This is Home Page</h1>
+            </Frontend>
+   
+</template>
+
+<script setup>
+      import Frontend from '@/Layouts/FrontendLayout.vue';
+
+      import { Head } from '@inertiajs/vue3';
+</script>
+
