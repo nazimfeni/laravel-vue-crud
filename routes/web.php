29dc696619229cb43e9abcdf2c97f60d2ea89b1a
nazimfeni <mdnazimuddin77@gmail.com>
2024-09-19 19:35:41 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
-<<<<<<< HEAD
+
 use App\Http\Controllers\ProductController;
-=======
->>>>>>> 5064d8fddc360dd207bae1b4e5411de258d951da
+
+
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\FrontendController;
 
@@ -14,12 +14,11 @@ Route::get('/',[FrontendController::class,'index'])->name('myhome');
 Route::get('/about',[FrontendController::class,'about'])->name('aboutUs');
 Route::inertia('/contact','Frontend/Contact')->name('contactUs');
 
-<<<<<<< HEAD
+
 Route::resource('products', ProductController::class);
 
 
-=======
->>>>>>> 5064d8fddc360dd207bae1b4e5411de258d951da
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
